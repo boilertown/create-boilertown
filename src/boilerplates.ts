b35354070d6df677b9af6d55c239175d72b76143
Quan Pham <6479147+mikunpham@users.noreply.github.com>
2022-07-22 11:29:45 +0700

    chore: change messages and display

--- src/boilerplates.ts
+++ src/boilerplates.ts
@@ -1,7 +1,8 @@
-export const boilerplates = [
+import type { Boilerplate } from './types.js';
+
+export const boilerplates: Boilerplate[] = [
 	{
 		name: 'nostalgia-boilerplate',
 		repo: 'https://github.com/boilertowns/nostalgia-boilerplate',
-		git: 'https://github.com/boilertowns/nostalgia-boilerplate.git',
 	},
 ];
