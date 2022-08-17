2b6f736f328e748df9a266008eb4162075fc676b
Quan Pham <6479147+mikunpham@users.noreply.github.com>
2022-08-17 23:44:17 +0700

    fix: correctly remove files

--- src/modifiers/cleanupFiles.ts
+++ src/modifiers/cleanupFiles.ts
@@ -16,13 +16,11 @@ export const cleanupFiles = (dir: string) => {
 	for (const file of TO_BE_REMOVED) {
 		const target = path.join(dir, file);
 
-		if (!fs.existsSync(target)) {
-			return;
+		if (fs.existsSync(target)) {
+			fs.rmSync(target, {
+				force: true,
+				recursive: true,
+			});
 		}
-
-		fs.rmSync(target, {
-			force: true,
-			recursive: true,
-		});
 	}
 };
