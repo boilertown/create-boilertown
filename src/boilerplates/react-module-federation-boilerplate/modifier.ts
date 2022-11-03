86da228d95d55fd560b15c43a9776e3b27e1facd
Pham Quan <6479147+mikunpham@users.noreply.github.com>
2022-11-03 15:51:27 +0700

    feat: add a new boilerplate (#52)
    * feat: add new boilerplate
    
    * chore: add changeset file

---
+++ src/boilerplates/react-module-federation-boilerplate/modifier.ts
@@ -0,0 +1,70 @@
+import { globbySync } from 'globby';
+import fs from 'node:fs';
+import path from 'node:path';
+import sortPackageJson from 'sort-package-json';
+import type { PackageJson } from 'type-fest';
+import { modifyGithubActions } from 'modifiers/index.js';
+import { getPkgManagerFromAgent } from 'utils/getPkgManagerFromAgent.js';
+import type { Modifier } from 'types/index.js';
+
+/**
+ * Custom modifier of https://github.com/boilertowns/react-module-federation-boilerplate
+ */
+export const modifier: Modifier = ({ projectDir }) => {
+	const { pkgManagerName, pkgManagerVersion } = getPkgManagerFromAgent();
+	const rootPackageJsonPath = path.join(projectDir, 'package.json');
+	const rootPackageJsonContent = JSON.parse(
+		fs.readFileSync(rootPackageJsonPath, 'utf-8'),
+	) as PackageJson;
+
+	(rootPackageJsonContent as any).packageManager = pkgManagerVersion
+		? `${pkgManagerName}@${pkgManagerVersion}`
+		: `${pkgManagerName}`;
+
+	const workspaces = rootPackageJsonContent.workspaces as string[];
+	const patterns = workspaces.map(
+		(workspace) => `${projectDir}/${workspace}/package.json`,
+	);
+	// rootDir/apps/*/package.json
+	const packageJsonFiles = globbySync(patterns);
+
+	/**
+	 * The original boilerplate is using `pnpm`. CLI need to modify this to support
+	 * `npm` and `yarn` properly.
+	 */
+	if (pkgManagerName !== 'pnpm') {
+		/**
+		 * Remove `pnpm-workspace.yaml`
+		 */
+		const pnpmWorkspaceFilePath = path.join(projectDir, 'pnpm-workspace.yaml');
+		if (fs.existsSync(pnpmWorkspaceFilePath)) {
+			fs.rmSync(pnpmWorkspaceFilePath, {
+				force: true,
+			});
+		}
+
+		/**
+		 * Replace packages in workspace version from "workspace:*" to  "*"
+		 */
+		for (const packageJsonPath of packageJsonFiles) {
+			const packageJsonContent = fs.readFileSync(packageJsonPath, {
+				encoding: 'utf-8',
+			});
+
+			const newPackageJsonContent = sortPackageJson(
+				packageJsonContent.replace(/workspace:\*/g, '*'),
+			);
+
+			fs.writeFileSync(packageJsonPath, newPackageJsonContent);
+		}
+
+		modifyGithubActions(projectDir);
+	} else {
+		rootPackageJsonContent.workspaces = undefined;
+	}
+
+	fs.writeFileSync(
+		rootPackageJsonPath,
+		sortPackageJson(JSON.stringify(rootPackageJsonContent, null, 2)),
+	);
+};
